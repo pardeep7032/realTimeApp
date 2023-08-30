5559238d0c896470d2786d1c5f8d597d421a0fbb
Pardeep <pardeep@gmail.com>
2023-08-30 22:41:35 +0530

    commits

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+   
+    extend: {
+      colors:{
+        'primary': "#1476ff",
+        'primary-light' : '#f9faff',
+        'secondary' : '#f3f5ff'
+      },
+    },
+  },
+  plugins: [],
+}
